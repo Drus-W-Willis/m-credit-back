2443e2c639c7269d7c3c2afe60f6d69fe02f5e0b
drus.willis30@gmail.com <drus.willis30@gmail.com>
2021-11-11 16:50:32 +0300

    roles in progress

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { AuthService } from './auth.service';
+import { User } from '../users/users.model';
 
-@ApiTags('Авторизация')
+@ApiTags('Авторизация и регистрация')
 @Controller('auth')
 export class AuthController {
 
   constructor(private authService: AuthService) {
   }
 
+  @ApiOperation({summary: 'Авторизация пользователя'})
+  @ApiResponse({status: 200, type: User})
   @Post('/login')
   login(@Body() userDto: CreateUserDto) {
     return this.authService.login(userDto)
   }
 
-
+  @ApiOperation({summary: 'Регистрация нового пользователя'})
+  @ApiResponse({status: 200, type: User})
   @Post('/registration')
   registration(@Body() userDto: CreateUserDto) {
     return this.authService.registration(userDto)
--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,18 +1,23 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Role } from './role.model';
 
 @ApiTags('Роли')
 @Controller('roles')
 export class RolesController {
   constructor(private rolesService: RolesService) {}
 
-  @Post()
+  @ApiOperation({summary: 'Добавление нововй роли'})
+  @ApiResponse({status: 200, type: Role})
+  @Post("/create")
   create(@Body() dto: CreateRoleDto) {
     return this.rolesService.createRole(dto)
   }
 
+  @ApiOperation({summary: 'Выбор роли по id'})
+  @ApiResponse({status: 200, type: Role})
   @Get('/:value')
   getByValue(@Param('value') value: string) {
     return this.rolesService.getRoleByValue(value)
